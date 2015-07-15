677dd058ea0a43a4358d2afd00088a7307f3c8cb
Ju Gonçalves <junia.goncalves@gmail.com>
2015-07-15 08:10:21 +0200

    Add Table file

---
+++ src/parsing/table.js
@@ -0,0 +1,25 @@
+import XLSX from 'xlsx';
+
+/**
+ * Like Layout, but for tables that are not plate layouts.
+ */
+class Table {
+
+    static async parse(sheet, {
+        required=[],
+        converters={},
+        validators={},
+        parallel=false
+        } = {}) {
+
+
+    }
+
+    append(contents) {
+
+    }
+
+    toSheet() {
+
+    }
+}
---
+++ test/table.js
@@ -0,0 +1,10 @@
+import {Layout} from '../src/parsing/layout.js';
+import {expect} from 'chai';
+
+describe('Table parsing', () => {
+
+    it('', () => {
+        expect(false).to.be.true;
+    });
+
+});
